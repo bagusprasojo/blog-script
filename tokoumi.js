650a6d809d37c33232944f1e445df2ab4e74d873
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2025-08-01 16:32:04 +0700

    update button

--- tokoumi.js
+++ tokoumi.js
@@ -85,7 +85,6 @@ fetch('https://www.tokoumi.com/feeds/posts/default/-/Terjual?alt=json&max-result
 
       col.innerHTML = `
         <div class='card product-card'>
-          <img src='${imgSrc}' class='card-img-top' alt='${title}' />
           <div class='card-body text-center'>
             <h5 class='card-title'>${title}</h5>            
             <div class='justify-content-between'>
@@ -93,12 +92,7 @@ fetch('https://www.tokoumi.com/feeds/posts/default/-/Terjual?alt=json&max-result
               <div class='btn-group'>
               <button type='button' class='btn btn-sm btn-outline-secondary dropdown-toggle' data-bs-toggle='dropdown'>
                 Share
-              </button>
-              <ul class='dropdown-menu dropdown-menu-end'>
-                <li><a class='dropdown-item' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}'>Facebook</a></li>
-                <li><a class='dropdown-item' target='_blank' href='https://wa.me/?text=${encodeURIComponent(title + " " + link)}'>WhatsApp</a></li>
-                <li><a class='dropdown-item' target='_blank' href='https://twitter.com/intent/tweet?text=${encodeURIComponent(title + " " + link)}'>Twitter</a></li>
-              </ul>
+              </button>              
             </div>
 
             </div>
