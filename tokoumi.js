eefdfe984f6f3ac81057aa05b1357749aedc06e2
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2025-08-03 17:07:22 +0700

    tambah tinggi dan lebar image

--- tokoumi.js
+++ tokoumi.js
@@ -28,7 +28,7 @@ fetch('https://www.tokoumi.com/feeds/posts/default/-/Produk?alt=json&max-results
 
       col.innerHTML = `
         <div class='card product-card'>
-          <img width="162" height="242"  src='${imgSrc}' class='card-img-top' alt='${title}' />
+          <img  src='${imgSrc}' class='card-img-top' alt='${title}' />
           <div class='card-body text-center'>
             <h5 class='card-title'>${title}</h5>            
             <div class='justify-content-between'>
